1ede23bca55ef118d17114ca14c903d334db7c32
Mark Larter <mark.larter@outlook.com>
2013-01-07 18:19:14 -0700

    Fix constructor parameter order.

--- jquery.ui.timeinput.js
+++ jquery.ui.timeinput.js
@@ -32,7 +32,7 @@
 		timeRegex = /^((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$/;
     
     // The plugin constructor.
-    var timeInput = function(element, options) {
+    var timeInput = function(options, element) {
         this.options = $.extend({}, defaults, options || {});
         this.element = element;
         
@@ -145,6 +145,3 @@
     };
     */
 })(jQuery);
-
-// Hook up to widget bridge.
-$.widget.bridge("fg", timeInput);
